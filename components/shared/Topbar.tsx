0d50cfe79b871615328769c7ba86555ec87e4949
Prakash kumar singh <Pk87096639@gmail.com>
2024-08-03 16:31:21 +0530

    created auth actions

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -13,6 +13,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
+import { dark } from "@clerk/themes";
+
 const Topbar = () => {
   const pathname = usePathname();
   return (
@@ -22,22 +24,10 @@ const Topbar = () => {
         <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
       </Link>
       <div className="flex items-center gap-1">
-        <div className="block md:hidden">
-          <SignedIn>
-            <SignOutButton>
-              <div className="flex cursor-pointer">
-                <Image
-                  src="/assets/logout.svg"
-                  alt="logout"
-                  width={24}
-                  height={24}
-                />
-              </div>
-            </SignOutButton>
-          </SignedIn>
-        </div>
+        <div className="block md:hidden"></div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
@@ -77,14 +67,22 @@ const Topbar = () => {
                   </Link>
                 );
               })}
+              <SignedIn>
+                <SignOutButton redirectUrl="/sign-in">
+                  <div className="flex items-center justify-start gap-2 p-2 bg-red-500 rounded-lg hover:bg-red-700">
+                    <div className="flex cursor-pointer">
+                      <Image
+                        src="/assets/logout.svg"
+                        alt="logout"
+                        width={20}
+                        height={20}
+                      />
+                    </div>
+                    <p className="text-light-2">Logout</p>
+                  </div>
+                </SignOutButton>
+              </SignedIn>
             </DropdownMenuContent>
-
-            {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem> */}
           </DropdownMenu>
         </div>
       </div>
---
+++ lib/validations/users.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+
+export const UserValidation = z.object({
+  profile_photo: z.string().url().nonempty(),
+  name: z
+    .string()
+    .min(3, { message: "Name should be minimum 3 characters" })
+    .max(20),
+  username: z
+    .string()
+    .min(3, { message: "Name should be minimum 3 characters" })
+    .max(20),
+  bio: z
+    .string()
+    .min(3, { message: "Name should be minimum 3 characters" })
+    .max(1000),
+});
