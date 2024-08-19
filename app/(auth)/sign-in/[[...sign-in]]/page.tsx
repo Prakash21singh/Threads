642860aefed8faa6ea389f66e93a5977e0a03c52
Prakash kumar singh <Pk87096639@gmail.com>
2024-08-19 07:43:34 +0530

    Created Search User functionality just have to filter it

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="w-full flex items-center justify-center h-screen bg-dark-3">
+      <div className="flex-1 h-full flex justify-center items-center">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
